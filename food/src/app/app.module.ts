14dc042fb740748b113f53c15b35037b6ec243a2
Maria fernanda <marifer3335@hotmail.com>
2018-05-29 13:48:33 -0500

    pagina de comida

---
+++ food/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    MenuComponent,
+    ContactoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
